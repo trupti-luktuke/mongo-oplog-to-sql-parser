ceac580f08b9ed67642f20ec4a39ab7f08b9032e
One2N <one2n@One2Ns-MacBook-Air.local>
2025-07-09 21:31:15 -0700

    Initial commit

---
+++ src/main/java/com/one2n/mongooplogtosqlparser/MongoOplogToSqlParserApplication.java
@@ -0,0 +1,13 @@
+package com.one2n.mongooplogtosqlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoOplogToSqlParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoOplogToSqlParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/one2n/mongooplogtosqlparser/MongoOplogToSqlParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one2n.mongooplogtosqlparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoOplogToSqlParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
